c0099fc7f1c8794cd49d765609a0dc51dc19202e
FWKCarlisle <fwkcarlisle@gmail.com>
2024-12-04 12:11:45 +0000

    Heat map perfect

--- Analysis code/DBTF/dat_to_heat_map.py
+++ Analysis code/DBTF/dat_to_heat_map.py
@@ -160,17 +160,20 @@ class Spectrum(output_data_spectra_dat):
     
 def create_map(X,Y, height, label='Height map', scale=1,angle=55):
 
+    map_fig, map_ax = plt.subplots()
     scatter = plt.scatter(X, Y, c=height, cmap='gist_rainbow', s=scale)
-    save_path = data_dir + f"\\height_map_{file}_{bias_data[0]:.3f}mV.png" 
-    plt.colorbar(scatter, label=label)
-    plt.xlabel('X (m)')
-    plt.ylabel('Y (m)')
-    plt.xlim(left=0)
-    plt.ylim(bottom=0)
-    plt.title('Height map')
-    plt.savefig(save_path)
+    save_path = data_dir + f"\\height_map_{file}_{bias_data[0]:.3f}mV.png"
+    map_ax.plot(np.full(100, np.mean(X)), np.linspace(np.min(Y), np.max(Y), 100), 'k--', label='Median X value')
+    map_fig.colorbar(scatter, label=label)
+    map_ax.set_xlabel('X (m)')
+    map_ax.set_ylabel('Y (m)')
+    map_ax.set_xlim(left=0)
+    map_ax.set_ylim(bottom=0)
+    map_ax.set_title('Height map')
+    map_fig.savefig(save_path)
     print(f"Saved to: {save_path}")
     # plt.show()
+    plt.close(map_fig)
     
     
     # return heatmap
@@ -204,49 +207,98 @@ def remove_small_values(data, threshold):
             data[i] = 0
     return data, index_list
 
-data_dir =r"C:\Users\ppxfc1\OneDrive - The University of Nottingham\Desktop\PhD\Data\DBTF_Heat_maps"
-file_names = [f.name for f in Path(data_dir).iterdir() if f.is_file()]
-file_names.sort()
+def line_profile(x, y, height, label, threshold_percentage = 1.5,):
 
-print(file_names)
-backgrounds_z = []
-backgrounds_current = []
-min_z_list = []
-min_current_list = []
-for i, file in enumerate(file_names):
-    if file[-1] == "t":
-        spectrum = Spectrum(data_dir, file, 'Bias (V)')
+    median_x = np.median(x) #median to avoid outliers
 
-        current_data, current_channel = spectrum.give_data('Current (A)')
-        bias_data, bias_channel = spectrum.give_data('Bias (V)')
-        x_data, x_channel = spectrum.give_data('X (m)')
-        y_data, y_channel = spectrum.give_data('Y (m)')
-        z_data, z_channel = spectrum.give_data('Z (m)')
+    lower_bound = median_x * (1-(threshold_percentage/100))  # 1% below the median
+    upper_bound = median_x * (1+(threshold_percentage/100))  # 1% above the median
 
-        rotated_x, rotated_y = rotate_map(x_data, y_data, 55)
 
-        min_z = np.min(z_data)
-        min_current = np.min(current_data)
-        backgrounds_z.append(min_z)
-        backgrounds_current.append(min_current)
+    x_indices_within_range = np.where((x >= lower_bound) & (x <= upper_bound))[0]
+    y_line = y[x_indices_within_range]
+    height_profile = height[x_indices_within_range]
 
-        if i == 0:
-            zero_points = np.min(rotated_x) , np.min(rotated_y)
-        rotated_x -= zero_points[0]
-        rotated_y -= zero_points[1]
+    indicies = np.argsort(y_line)
+    y_line = y_line[indicies]
+    height_profile = height_profile[indicies]
+
+    global line_ax
+
+    line_ax.plot(y_line, height_profile, label = label, alpha=0.8)
+    
+    return y_line, height_profile
+
+
+def analysis_func(x, y, height, reference_height, label, scale=1, threshold_percentage = 10):
+        # % of max value, aritrary threshold, can be changed but this is just to prove the concept
+        reference_height_data, index_list = remove_small_values(reference_height, 0)
         
-        z_data = z_data - min_z
-        current_data = current_data - min_current
-        min_z_list.append(np.min(z_data))
-        min_current_list.append(np.min(current_data))
-
-        z_data, index_list = remove_small_values(z_data, z_data.max() * 0.01)
-        print(len(index_list)) # 50% of max value, aritrary threshold, can be changed but this is just to prove the concept
         
-        current_data = np.delete(current_data, index_list)
-        rotated_x = np.delete(rotated_x, index_list)
-        rotated_y = np.delete(rotated_y, index_list)
+        height = np.delete(height, index_list)
+        x = np.delete(x, index_list) #Remove out of threshold values
+        y = np.delete(y, index_list)
 
-        create_map(rotated_x, rotated_y, current_data, label="Current (A)", scale=1)
+        
+
+        create_map(x, y, height, label, scale)
+        y_line, height_profile = line_profile(x, y,height, label=label)
+        return y_line, height_profile
+
+        
+if __name__ == "__main__":
+    data_dir =r"C:\Users\ppxfc1\OneDrive - The University of Nottingham\Desktop\PhD\Data\DBTF_Heat_maps"
+    file_names = [f.name for f in Path(data_dir).iterdir() if f.is_file()]
+    file_names.sort()
+
+    print(file_names)
+    backgrounds_z = []
+    backgrounds_current = []
+    min_z_list = []
+    min_current_list = []
+    y_list = []
+    height_list = []
+
+    fig, line_ax = plt.subplots()
+    for i, file in enumerate(file_names):
+        if file[-1] == "t":
+            spectrum = Spectrum(data_dir, file, 'Bias (V)')
+
+            current_data, current_channel = spectrum.give_data('Current (A)')
+            bias_data, bias_channel = spectrum.give_data('Bias (V)')
+            x_data, x_channel = spectrum.give_data('X (m)')
+            y_data, y_channel = spectrum.give_data('Y (m)')
+            z_data, z_channel = spectrum.give_data('Z (m)')
+
+            min_z = np.min(z_data)
+            min_current = np.min(current_data)
+            backgrounds_z.append(min_z)
+            backgrounds_current.append(min_current)
 
 
+            z_data = z_data - min_z
+            current_data = current_data - min_current
+            min_z_list.append(np.min(z_data))
+            min_current_list.append(np.min(current_data))
+            
+            rotated_x, rotated_y = rotate_map(x_data, y_data, 55)
+
+            if i == 0:
+                zero_points = np.min(rotated_x) , np.min(rotated_y)
+            rotated_x -= zero_points[0]
+            rotated_y -= zero_points[1]
+
+            
+            y_line, height_profile = analysis_func(rotated_x, rotated_y,z_data, current_data, label=file, scale=1)
+            y_list.append(y_line)
+            height_list.append(height_profile)
+
+
+    
+
+line_ax.set_title("Line profile")
+line_ax.set_xlabel("Y axis")
+line_ax.set_ylabel("Z")
+line_ax.legend()  # Add a legend to distinguish plots
+# plt.grid(True)
+plt.show()
